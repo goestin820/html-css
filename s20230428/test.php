6b326faaf001aa45e2755260fab8bdce099c7e75
kai <cgk0910@gmail.com>
2023-05-10 14:43:16 +0800

    js vs php string

--- s20230428/test.php
+++ s20230428/test.php
@@ -1,4 +1,25 @@
 <?php 
     $myArr = [];
+    
+    $sum = 0;
+    for ($i=1; $i <=10 ; $i++) { 
+        $test = 'name';
+        $sum = $sum + $i;
 
+    }
+
+    // php 
+    // 1. 字串相加 .
+    // 2. 字串內加變數
+    $name = 'name';
+    $name = "test name";
+    // $myString = 'Hello $name';
+    // 1. 字串相加 .
+    $myString = "Hello " . name;
+    // 2. 字串內加變數
+    $myString = "Hello $name";
+
+
+
+    echo $test;
 ?>
